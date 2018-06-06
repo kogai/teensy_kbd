ec64f4b821c1475f0684481f6229d5da9537f171
Shinichi Kogai <kogai0121@gmail.com>
2018-06-06 21:39:55 +0900

    Impl Fei

--- src/clock.rs
+++ src/clock.rs
@@ -109,3 +109,81 @@ impl Mcg {
         while self.s.read().get_bits(2..4) != 3 {}
     }
 }
+
+pub struct Fei {
+    mcg: &'static mut Mcg,
+}
+
+pub struct Fbe {
+    mcg: &'static mut Mcg,
+}
+
+pub struct Pbe {
+    mcg: &'static mut Mcg,
+}
+
+pub enum OscRange {
+    Low = 0,
+    High = 1,
+    VeryHigh = 2,
+}
+
+pub enum OscSource {
+    LockedLoop = 0,
+    Intenal = 1,
+    External = 2,
+}
+
+impl Fei {
+    pub fn enable_xtal(&mut self, range: OscRange) {
+        self.mcg.c2.update(|c2| {
+            c2.set_bits(4..6, range as u8);
+            c2.set_bit(2, true);
+        });
+        // Wait for the crystal oscillator to become enabled.
+        while !self.mcg.s.read().get_bit(1) {}
+    }
+
+    pub fn use_external(self, divide: u32) -> Fbe {
+        let osc = self.mcg.c2.read().get_bits(4..6);
+        let frdiv = if osc == OscRange::Low as u8 {
+            match divide {
+                1 => 0,
+                2 => 1,
+                4 => 2,
+                8 => 3,
+                16 => 4,
+                32 => 5,
+                64 => 6,
+                128 => 7,
+                _ => panic!("Invalid external clock divider: {}", divide),
+            }
+        } else {
+            match divide {
+                32 => 0,
+                64 => 1,
+                128 => 2,
+                256 => 3,
+                512 => 4,
+                1024 => 5,
+                1280 => 6,
+                1536 => 7,
+                _ => panic!("Invalid external clock divider: {}", divide),
+            }
+        };
+        self.mcg.c1.update(|c1| {
+            c1.set_bits(6..8, OscSource::External as u8);
+            c1.set_bits(3..6, frdiv);
+            c1.set_bit(2, false);
+        });
+
+        // Once we write to the control register, we need to wait for
+        // the new clock to stabilize before we move on.
+        // First: Wait for the FLL to be pointed at the crystal
+        // Then: Wait for our clock source to be the crystal osc
+        while self.mcg.s.read().get_bit(4) {}
+        while self.mcg.s.read().get_bits(2..4) != OscSource::External as u8 {}
+
+        Fbe { mcg: self.mcg }
+    }
+}
